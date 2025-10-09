1c7f7420cc99d27e3cfa6fe290e5ee49537a3a37
Jromanosorio <jandres.roman.08@gmail.com>
2025-10-09 12:43:40 -0500

    prop object added

--- app/components/form/add.costumer.tsx
+++ app/components/form/add.costumer.tsx
@@ -1,4 +1,5 @@
 import { useLocalStorage } from "@/app/hooks/useLocalStorage";
+import { Costumer } from "@/app/interfaces/Costumer";
 import { LoginResponse } from "@/app/interfaces/Login";
 import { addCostumer } from "@/app/services/costumers";
 import { Button } from "@/components/ui/button";
@@ -7,7 +8,8 @@ import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
 
 interface formProps {
-    onCreateCostumer: (id: string, name: string, email: string, address: string) => void
+    error: string | null;
+    onCreateCostumerFn: (costumer: Costumer) => void
 }
 
 export default function AddCostumerForm(props: formProps) {
@@ -18,7 +20,7 @@ export default function AddCostumerForm(props: formProps) {
 
     const createCostumer = (e: React.FormEvent) => {
         e.preventDefault()
-        props.onCreateCostumer(id, name, email, address)
+        props.onCreateCostumerFn({id, name, email, address})
 
         setId("")
         setName("")
@@ -48,6 +50,7 @@ export default function AddCostumerForm(props: formProps) {
                     <Label className="text-sm font-medium text-gray-700 mb-2">Direccion<span className="text-red-500">*</span></Label>
                     <Input onChange={(e) => { setAddress(e.target.value) }} required className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent text-sm" type="string" value={address} />
                 </div>
+                {props.error && <span className="text-red-500 my-2 text-sm font-bold">{props.error}</span>}
                 <div>
                     <Button type="submit" className="cursor-pointer w-full mt-2">Guardar</Button>
                 </div>
